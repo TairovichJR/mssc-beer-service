ed46475152a514fac3bc7dc2c3593fb3d4f63062
TairovichJR <yokubov.omonjon@gmail.com>
2021-03-30 20:52:37 -0400

    hikari config

--- src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
@@ -2,22 +2,20 @@ package guru.springframework.msscbeerservice.bootstrap;
 
 import guru.springframework.msscbeerservice.domain.Beer;
 import guru.springframework.msscbeerservice.repositories.BeerRepository;
+import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.UUID;
 
-//@Component
+@Component
 public class BeerLoader implements CommandLineRunner {
 
     public static final String BEER_1_UPC = "0631234200036";
     public static final String BEER_2_UPC = "0631234300019";
     public static final String BEER_3_UPC = "0083783375213";
-
-    public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
-    public static final UUID BEER_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
-    public static final UUID BEER_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
 
     private final BeerRepository repository;
 
@@ -34,7 +32,7 @@ public class BeerLoader implements CommandLineRunner {
         if (repository.count() == 0){
             Beer beer1 = Beer.builder()
                     .beerName("Mango Bobs")
-                    .beerStyle("IPA")
+                    .beerStyle(BeerStyleEnum.IPA.name())
                     .upc(BEER_1_UPC)
                     .price(new BigDecimal("12.95"))
                     .quantityToBrew(200)
@@ -43,7 +41,7 @@ public class BeerLoader implements CommandLineRunner {
 
             Beer beer2 = Beer.builder()
                     .beerName("Galaxy Cat")
-                    .beerStyle("PALE_ALE")
+                    .beerStyle(BeerStyleEnum.PALE_ALE.name())
                     .upc(BEER_2_UPC)
                     .price(new BigDecimal("11.95"))
                     .quantityToBrew(200)
@@ -51,8 +49,8 @@ public class BeerLoader implements CommandLineRunner {
                     .build();
 
             Beer beer3 = Beer.builder()
-                    .beerName("No Hammers on the Bar")
-                    .beerStyle("PALE_ALE")
+                    .beerName("Pinball Porter")
+                    .beerStyle(BeerStyleEnum.PALE_ALE.name())
                     .upc(BEER_3_UPC)
                     .price(new BigDecimal("15.95"))
                     .quantityToBrew(200)
